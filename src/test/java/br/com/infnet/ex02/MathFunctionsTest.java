15dfd906decd00a91744a0e87dff5bc575322a48
Arthur-Fortunato <arthur.r.t.fortunato@gmail.com>
2025-12-01 15:49:31 -0300

    adição de teste da função GenerateMultiplicationTable

--- src/test/java/br/com/infnet/ex02/MathFunctionsTest.java
+++ src/test/java/br/com/infnet/ex02/MathFunctionsTest.java
@@ -10,4 +10,23 @@ public class MathFunctionsTest {
         int resultado = MathFunctions.MultiplyByTwo(numero);
         assertEquals(0, resultado % 2);
     }
+
+    @Property
+    void deveValidarQueTodosElementosSaoMultiplosDoNumeroOriginal(@ForAll("numerosNaoNegativosENaoNulos") int numero, @ForAll("numerosParaLimite") int limite){
+        int[] numeros = MathFunctions.GenerateMultiplicationTable(numero, limite);
+        for (int valor : numeros) {
+            assertEquals(0, valor % numero);
+        }
+    }
+
+    @Provide
+    Arbitrary<Integer> numerosNaoNegativosENaoNulos(){
+        return Arbitraries.integers().between(1, 1_000_000);
+    }
+
+    @Provide
+    Arbitrary<Integer> numerosParaLimite() {
+        return Arbitraries.integers().between(1, 100);
+    }
+
 }
